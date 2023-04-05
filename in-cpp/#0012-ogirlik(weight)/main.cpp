e41df1698ce80518beccd0bca5ff11fc9f175097
Zayniddin <zayniddin.saydillaev@gmail.com>
2023-04-06 04:46:43 +0500

    solutions in cpp

---
+++ in-cpp/#0012-ogirlik(weight)/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    double a;
+    cin >> a;
+    cout.precision(2); cout<<fixed;
+    cout <<a * 9.8;
+    return 0;
+}
---
+++ in-cpp/#0014-tok_kuchi/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+    float u, r;
+    cin >>u >>r;
+    cout.precision(3); cout <<fixed;
+    float i = u/r;
+    cout <<i;
+}
