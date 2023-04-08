b66611dc58f677712efb942ba440de736ac78cb0
Zayniddin <zayniddin.saydillaev@gmail.com>
2023-04-09 00:09:41 +0500

    added #0034-tarmoqlanuvchi4.cpp
    solution to test 0034

---
+++ in-cpp/#0034-tarmoqlanuvchi4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+    int a, b, c;
+    cin >>a >>b >>c;
+    if (a < b && b < c) {cout  <<"YES"; }
+    else {cout <<"NO"; };
+    return 0;
+}
