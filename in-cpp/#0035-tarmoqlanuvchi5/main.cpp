3cfd53641e03c4f2b2bf1c143d6120d1a8097ac8
Zayniddin <zayniddin.saydillaev@gmail.com>
2023-04-11 00:13:02 +0500

    #0035-tarmoqlanuvchi.cpp
    added solution to 0035

---
+++ in-cpp/#0035-tarmoqlanuvchi5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b,c,s,d,e;
+    cin>>a>>b>>c;
+    a1=abs(a);
+    b1=abs(b);
+    c1=abs(c);
+    if (a>=b && b>=c) cout<<a*2<<" "<<b*2<<" "<<c*2;
+    else cout<<a1<<" "<<b1<<" "<<c1;
+    return 0;
+}
